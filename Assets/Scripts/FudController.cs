5daa9a55b0123b39ba886c012f447ee423068b18
bosstweed11 <clowntown7000@protonmail.com>
2021-10-06 13:56:01 +0300

    dev: Add Player movement
    added sprite and movement

--- Assets/Scripts/FudController.cs
+++ Assets/Scripts/FudController.cs
@@ -1,11 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FudController : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+    private Vector2 _moveInput;
+    private Rigidbody2D _rigidbody2D;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        gameObject.transform.Rotate(0,0,5);
+        Run();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2
+        {
+            x = _moveInput.x * _moveSpeed,
+            y = _rigidbody2D.velocity.y
+        };
+        _rigidbody2D.velocity = playerVelocity;
+    }
+
+    void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
     }
 }
